a0049edcadaa17209189f6c69aaf46a181030858
Silvana Barraza <barrazasilvana@gmail.com>
2023-03-07 15:39:36 -0300

    Se corrigieron errores ortograficos, se agrego un submenu en el header

--- js/main.js
+++ js/main.js
@@ -18,9 +18,9 @@ function modificar_parrafo (parrafo){
 
 //variables
 
-const about_intro='Soy diseñadora UX/UI, desarrolladora web frontend y también licenciada en nutrición. Durante la pandemia del 2020, tuve la oportunidad de conocer y adentrarme en este campo del dieño. Descubrí una nueva vocación, por lo que mi objetivo es poder seguir este camino laboral y profesionalmente. Capacitandome, adquiriendo nuevas habilidades y llevandolas a la práctica. ';
+const about_intro='Soy diseñadora UX/UI, desarrolladora web frontend y también licenciada en nutrición. Durante la pandemia del 2020, tuve la oportunidad de conocer y adentrarme en este campo del diseño. Descubrí una nueva vocación, por lo que mi objetivo es poder seguir este camino laboral y profesionalmente. Capacitandome, adquiriendo nuevas habilidades y llevandolas a la práctica. ';
 
-const about_me=`Soy diseñadora UX/UI, desarrolladora web frontend y también licenciada en nutrición. Durante la pandemia del 2020, tuve la oportunidad de conocer y adentrarme en este campo del dieño. Descubrí una nueva vocación, por lo que mi objetivo es poder seguir este camino laboral y profesionalmente. Capacitandome, adquiriendo nuevas habilidades y llevandolas a la práctica. Y por qué no, cuando sea oportuno, darle mi persepectiva en relación a la salud y la nutricón. Me gustaria formar parte de un equipo que me permita aplicar mis conocimientos, aprender y crecer profesioanlmente. Me gusta poner atención los detalles y lograr resultados equilibrados y funcionales.`;
+const about_me=`Soy diseñadora UX/UI, desarrolladora web frontend y también licenciada en nutrición. Durante la pandemia del 2020, tuve la oportunidad de conocer y adentrarme en este campo del diseño. Descubrí una nueva vocación, por lo que mi objetivo es poder seguir este camino laboral y profesionalmente. Capacitandome, adquiriendo nuevas habilidades y llevandolas a la práctica. Y por qué no, cuando sea oportuno, darle mi persepectiva en relación a la salud y la nutricón. Me gustaria formar parte de un equipo que me permita aplicar mis conocimientos, aprender y crecer profesioanlmente. Me gusta poner atención los detalles y lograr resultados equilibrados y funcionales.`;
 
 
 let boton = document.getElementById("boton");
