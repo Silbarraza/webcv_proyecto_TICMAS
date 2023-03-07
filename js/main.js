8f6bc2bbdff3a56ed6625f7b932a0a8d36eedb46
Silvana Barraza <barrazasilvana@gmail.com>
2023-03-07 17:26:51 -0300

    se hicieron correcciones en la seccion sobre mi en htm y js, y se agregaron estilos al nuevo parrafo

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ console.log("Mi cv")
 
 //Funciones
 
-
 function ocultar_elemento(elemento){
     elemento.style.display='none'
 }
@@ -12,42 +11,46 @@ function mostrar_elemento(elemento){
     elemento.style.display='inline-block'
 }
 
-function modificar_parrafo (parrafo){
-    document.getElementById('about_me').innerHTML=parrafo;
+function modificar_parrafo (nuevo_parrafo){
+    document.getElementById('ver_mas').innerHTML= nuevo_parrafo;
+    
 }
+
 
 //variables
 
-const about_intro='Soy diseñadora UX/UI, desarrolladora web frontend y también licenciada en nutrición. Durante la pandemia del 2020, tuve la oportunidad de conocer y adentrarme en este campo del diseño. Descubrí una nueva vocación, por lo que mi objetivo es poder seguir este camino laboral y profesionalmente. Capacitandome, adquiriendo nuevas habilidades y llevandolas a la práctica. ';
+const parrafo_extendido = `Me gusta poner atención en los detalles y lograr resultados equilibrados y funcionales. Soy una persona amigable y responsable, con buenas relaciones interpersonales, por lo que me gustaría formar parte de un equipo que me permita aplicar mis conocimientos, aprender y crecer profesionalmente.`;
 
-const about_me=`Soy diseñadora UX/UI, desarrolladora web frontend y también licenciada en nutrición. Durante la pandemia del 2020, tuve la oportunidad de conocer y adentrarme en este campo del diseño. Descubrí una nueva vocación, por lo que mi objetivo es poder seguir este camino laboral y profesionalmente. Capacitandome, adquiriendo nuevas habilidades y llevandolas a la práctica. Y por qué no, cuando sea oportuno, darle mi persepectiva en relación a la salud y la nutricón. Me gustaria formar parte de un equipo que me permita aplicar mis conocimientos, aprender y crecer profesioanlmente. Me gusta poner atención los detalles y lograr resultados equilibrados y funcionales.`;
-
+const parrafo_vacio = " ";
 
 let boton = document.getElementById("boton");
 
 let boton_ocultar = document.getElementById("boton_ocultar");
 
-
-
 //Código
-
 
 ocultar_elemento(boton_ocultar);
 
-modificar_parrafo(about_intro)
-
 
 boton.addEventListener('click', function(){
-    
+    //al hacer click ocultamos el boton ver mas y mostramos el texto extendido
+
     ocultar_elemento(boton);
-    modificar_parrafo(about_me)
+
+    //modificar_parrafo(about_me,parrafo_intro)
+
+    modificar_parrafo(parrafo_extendido)
+    
+    //mostramos boton ver menos
     mostrar_elemento(boton_ocultar);
 
 });
 
 boton_ocultar.addEventListener('click',function(){
     ocultar_elemento(boton_ocultar);
-    modificar_parrafo(about_intro)
+
+    modificar_parrafo(parrafo_vacio)
+
     mostrar_elemento(boton)
 })
 
